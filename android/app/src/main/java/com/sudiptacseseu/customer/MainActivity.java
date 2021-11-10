d3093691e0d5610d497ea72474619208bbfa190f
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-11-10 11:09:12 +0600

    Method channel configured and queries added on manifest

--- android/app/src/main/java/com/sudiptacseseu/customer/MainActivity.java
+++ android/app/src/main/java/com/sudiptacseseu/customer/MainActivity.java
@@ -1,7 +1,41 @@
 package com.sudiptacseseu.customer;
 
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+
+    private static final String CHANNEL = "myChannel";
+
+    // Configuring flutter engine to use method channel
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+                        if (call.method.equals("callPhone")) {
+                            String text = call.argument("phone");
+                            String greetings = callPhone(text);
+                            result.success(greetings);
+                        }
+                    }});
+    }
+
+    // Native action
+    private String callPhone( String text) {
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:"+text));
+        startActivity(callIntent);
+        return "Calling "+text;
+    }
 
 }
